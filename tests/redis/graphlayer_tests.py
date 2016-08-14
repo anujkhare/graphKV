c94f08f6118175611593a2d19d1785799356c42a
Anuj Khare <khareanuj18@gmail.com>
2016-08-14 20:08:30 +0530

    test/graphlayer: added tests for multiple adds

--- tests/redis/graphlayer_tests.py
+++ tests/redis/graphlayer_tests.py
@@ -33,3 +33,31 @@ class TestGraphLayerRedis():
         assert foo_a == r_foo_a
         assert set(bar_a) == r_bar_a
         assert set([foo_b.encode()]) == r_foo_b
+
+    # since internally we store sets, no duplicate values should arise.
+    def test_graphlayer_redis_set_multiple_edges_duplicate_add(self):
+        foo_a = set([b'1', b'2', b'3'])
+        foo_b = '11'
+        bar_a = [b'11', b'22']
+        l = {
+            'foo': {'a': foo_a, 'b': foo_b},
+            'bar': {'a': bar_a}
+            }
+        self.gr.set_multiple_edges(**l)
+        self.gr.set_multiple_edges(**l)
+
+        r_foo_a = self.r.smembers('foo:a')
+        r_foo_b = self.r.smembers('foo:b')
+        r_bar_a = self.r.smembers('bar:a')
+        assert foo_a == r_foo_a
+        assert set(bar_a) == r_bar_a
+        assert set([foo_b.encode()]) == r_foo_b
+
+    def test_graphlayer_redis_set_multiple_edges_multiple_add(self):
+        l1 = {'foo': {'a': ['1', '2']}}
+        l2 = {'foo': {'a': [b'3', b'2']}}
+        self.gr.set_multiple_edges(**l1)
+        self.gr.set_multiple_edges(**l2)
+
+        r_foo_a = self.r.smembers('foo:a')
+        assert set([b'1', b'2', b'3']) == r_foo_a
